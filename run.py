6107c4aca7c75dc02d86cd15b8732e59b0e0c423
valerik1983-sudo <valerik1983@gmail.com>
2026-07-04 19:25:34 +0300

    Add files via upload

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 3000))
     uvicorn.run(
-        "app.app.main:app",  # правильный путь к FastAPI приложению
+        "app.main:app",  # правильный путь к FastAPI приложению
         host="0.0.0.0",
         port=port,
         reload=False  # для продакшена лучше без reload
