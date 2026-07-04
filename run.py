43f626db4c13ad968d9cfe8e71d651828c5efe60
valerik1983-sudo <valerik1983@gmail.com>
2026-07-04 09:00:04 +0300

    Add files via upload

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
-import asyncio
-from app.main import bot, dp, init_db
-
-async def main():
-    init_db()
-    await bot.delete_webhook()
-    await dp.start_polling(bot)
+import uvicorn
+import os
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    port = int(os.getenv("PORT", 3000))
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=True  # для разработки, можно убрать на проде
+    )
